a22808793c1df0825818fba21cc98ff13d99893f
pengtao.geng <pengtao.geng@qunar.com>
2016-04-26 15:42:25 +0800

    add test

--- src/test/java/com/gpengtao/test/guava/GuavaTest.java
+++ src/test/java/com/gpengtao/test/guava/GuavaTest.java
@@ -1,6 +1,7 @@
 package com.gpengtao.test.guava;
 
 import com.google.common.base.Predicate;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import org.junit.Test;
@@ -16,7 +17,7 @@ public class GuavaTest {
     public void test_Iterable() {
         List<Integer> list = Lists.newArrayList(0, 1, 9, 2, 0, 2, 8, 7, 1, 2, 10, 3, 4);
         Iterables.removeIf(list, new Predicate<Integer>() {
-            @Override
+
             public boolean apply(Integer input) {
                 if (input > 3 || input < 2) {
                     return true;
@@ -27,5 +28,11 @@ public class GuavaTest {
         });
 
         System.out.println(list);
+    }
+
+    @Test
+    public void test_split_empty_string() {
+        Iterable<String> split = Splitter.on(",").split("");
+        System.out.println(split);
     }
 }
---
+++ src/test/java/com/gpengtao/test/java/test/ParentClass.java
@@ -0,0 +1,11 @@
+package com.gpengtao.test.java.test;
+
+/**
+ * Created by pengtao.geng on 2016/4/22.
+ */
+public abstract class ParentClass {
+
+    public String getName() {
+        return this.getClass().getName();
+    }
+}
