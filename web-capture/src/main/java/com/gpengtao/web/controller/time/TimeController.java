650f01976d63c8433c0bc16419628f64bdbd1ba4
gpengtao <513023768@qq.com>
2016-08-31 00:13:18 +0800

    modify

--- web-capture/src/main/java/com/gpengtao/web/controller/time/TimeController.java
+++ web-capture/src/main/java/com/gpengtao/web/controller/time/TimeController.java
@@ -1,5 +1,6 @@
 package com.gpengtao.web.controller.time;
 
+import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by pengtao.geng on 2016/8/29.
@@ -17,11 +19,15 @@ public class TimeController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping("/now")
-    public String hello() {
+    @RequestMapping(value = "/now")
+    public Map<String, Object> hello() {
         logger.info("访问now接口");
 
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return format.format(new Date());
+        String dateString = format.format(new Date());
+
+        Map<String, Object> map = Maps.newConcurrentMap();
+        map.put("hello", dateString);
+        return map;
     }
 }
