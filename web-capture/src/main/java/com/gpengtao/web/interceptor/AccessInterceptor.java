cb7e00182904f541ebf31b41f49465d6ad4d298d
pengtao.geng <pengtao.geng@qunar.com>
2016-09-09 15:20:42 +0800

    add freemaker

--- web-capture/src/main/java/com/gpengtao/web/interceptor/AccessInterceptor.java
+++ web-capture/src/main/java/com/gpengtao/web/interceptor/AccessInterceptor.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by gpengtao on 16/8/29.
  */
-public class AccessInterceptor implements HandlerInterceptor {
+public class AccessInterceptor extends HandlerInterceptorAdapter {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -20,15 +21,5 @@ public class AccessInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         logger.info("access interceptor,url: {}", request.getRequestURI());
         return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
     }
 }
