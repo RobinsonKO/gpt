3ca405112fd5db540b2d341af018b90a8753a397
gpengtao <513023768@qq.com>
2016-09-20 08:38:41 +0800

    add

--- drools-test/src/main/java/com/gpengtao/main/GlobalTest.java
+++ drools-test/src/main/java/com/gpengtao/main/GlobalTest.java
@@ -1,11 +1,24 @@
 package com.gpengtao.main;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
 /**
  * Created by gpengtao on 16/9/19.
  */
 public class GlobalTest {
 
     public static void main(String[] args) {
+        KieServices services = KieServices.Factory.get();
 
+        KieContainer container = services.getKieClasspathContainer();
+
+        KieSession session = container.newKieSession("ksession-global-test-rules");
+
+        session.insert(new Object());
+        session.fireAllRules();
+
+        session.dispose();
     }
 }
